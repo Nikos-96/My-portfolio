77a9f006958a4b73671467be3a4e3261124506d7
Nikos-96 <Nikos7331@gmail.com>
2023-02-06 19:35:28 +0100

    added footer links

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,29 @@
 import React from 'react';
 import './Footer.css';
+import { AiFillGithub } from 'react-icons/ai';
+import { BsTelegram } from 'react-icons/bs';
 
 const Footer = () => {
     return (
         <div className='footer'>
-
+            <a
+                className='footer-link'
+                href='https://github.com/Nikos-96/My-portfolio'
+                target='_blank'
+                rel='noreferrer'
+            >
+                <AiFillGithub size={50} color='white' />
+                GitHub
+            </a>
+            <a
+                className='footer-link'
+                href='https://telegram.me/Nikos_96'
+                target='_blank'
+                rel='noreferrer'
+            >
+                <BsTelegram size={50} color='white' />
+                Telegram
+            </a>
         </div>
     );
 };
