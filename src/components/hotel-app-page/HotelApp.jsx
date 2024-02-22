ca53e26b095b57d3345d4e7c55aaa762d09b93bd
Nikos-96 <Nikos7331@gmail.com>
2024-02-22 17:28:55 +0100

    update

--- src/components/hotel-app-page/HotelApp.jsx
+++ src/components/hotel-app-page/HotelApp.jsx
@@ -12,6 +12,9 @@ const HotelApp = () => {
                     You can download it here: <a href="download.php?file=Hotel.zip">Download Hotel.zip</a>
                 </p>
                 <p>It's an executable JAR file that requires Java 21 to run.</p>
+                <p>
+                    <b><i>Note:</i></b> Currently, the connection between the app and the server doesn't work because of the hosting side's CORS policy.
+                </p>
             </div>
             <diov className="hotel-app__image">
                 <img src="/img/hotel-app.png" alt="hotel app screenshot" />
